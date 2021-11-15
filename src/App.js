ec66d973961ed3e13d371b589026ad160d0da9df
Ashraf Kamel <a.kamel@karma-systems.com>
2021-11-15 23:22:18 +0200

    FunComp

--- src/App.js
+++ src/App.js
@@ -3,46 +3,43 @@ import {Route, Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SearchPage from './pages/SearchPage'
 import * as BooksAPI from './BooksAPI'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
-    books:[]
-  }
-  updateShelf = (book, shelf) => {
+const BooksApp = ()=>{
+let [books, setBooks] = useState([]);
+
+useEffect(()=>{
+  BooksAPI.getAll().then((booksResult) => {
+    setBooks(booksResult)
+})});
+
+ const updateShelf = (book, shelf) => {
     BooksAPI.update(book, shelf).then(() => {
-      BooksAPI.getAll().then((books) => {
-       this.setState({ books: books });
+      BooksAPI.getAll().then((booksResult) => {
+       setBooks(booksResult);
        })
     });
  
    }
-   componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books: books })
-    })
-
-  }
-  render() {
-    console.log(this.state.books);
-  return (
+ return (
+    
 <div className="app">
     <Routes>
           <Route exact path = "/"  element = {
             <HomePage 
-              books = {this.state.books}
-              updateShelf = {this.updateShelf}
+              books = {books}
+              updateShelf = {updateShelf}
             />
             }/>
          <Route path="/search" exact element = {
           <SearchPage
-           books={this.state.books}
-            updateShelf={this.updateShelf}
+           books={books}
+            updateShelf={updateShelf}
           />
          }/>
       </Routes>
     </div>
   )
-}
 }
 export default BooksApp
